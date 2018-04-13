6cb259085e2e38f946764eb61cd5de44f52aed31
翁霄驰(xiaochi weng) <wxc267@gmail.com>
2018-04-13 11:24:11 -0700

    Aidio Extractor

---
+++ src/multimediaquery/Complex.java
@@ -0,0 +1,55 @@
+package extractor;
+
+public class Complex {
+    private final double real; // the real part
+    private final double imag; // the imaginary part
+
+    public Complex(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    public Complex plus(Complex b) {
+        Complex a = this; // invoking object
+        double real = a.real + b.real;
+        double imag = a.imag + b.imag;
+        return new Complex(real, imag);
+    }
+
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double real = a.real - b.real;
+        double imag = a.imag - b.imag;
+        return new Complex(real, imag);
+    }
+
+    public Complex times(Complex b) {
+        Complex a = this;
+        double real = a.real * b.real - a.imag * b.imag;
+        double imag = a.real * b.imag + a.imag * b.real;
+        return new Complex(real, imag);
+    }
+
+    // return the real part
+    public double real_part() {
+        return this.real;
+    }
+
+    //return imagin part
+    public double imagin_part() {
+        return this.imag;
+    }
+
+    @Override
+    public boolean equals(Object x) {
+        if (x == null) {
+            return false;
+        }
+        if (this.getClass() != x.getClass()) {
+            return false;
+        }
+        Complex that = (Complex) x;
+        return (this.real == that.real) && (this.imag == that.imag);
+    }
+
+}
